692f9fbe255aa5f07f373841d9d90d06c5b277ff
kafuuma <arkafuuma@gmail.com>
2020-02-19 23:52:06 -0500

    separate business logic

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,36 +1,13 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
-import * as AWS from 'aws-sdk'
 import { createLogger } from '../../utils/logger'
-import {parseUserId} from '../../auth/utils'
+import { getTodos } from '../../businessLogic/todos'
 
-
-const todosTable = process.env.TODOS_TABLE
-const indexName = process.env.INDEX_NAME
-
-const docClient = new AWS.DynamoDB.DocumentClient()
 const logger = createLogger('get-todo')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   logger.info('Processing event: ', event)
-
-  const authHeader = event.headers.Authorization
-  const authSplit = authHeader.split(" ")
-  const token = authSplit[1]
-  const userId = parseUserId(token)
-
-
-  const result = await docClient.query({
-    TableName: todosTable,
-    IndexName: indexName,
-    KeyConditionExpression: 'userId = :userId',
-    ExpressionAttributeValues: {
-        ':userId': userId
-      }
-  }).promise()
-
-  const items = result.Items
 
   return {
     statusCode: 200,
@@ -38,7 +15,7 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       'Access-Control-Allow-Origin' : '*'
     },
     body: JSON.stringify({
-      items
+      items: await getTodos(event)
     })
   }
 }
